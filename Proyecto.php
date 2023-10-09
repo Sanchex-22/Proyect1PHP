c14f85199d10723401afcb57de6dedc144d432ef
Sanchex-22 <David21.cs70@gmail.com>
2023-10-09 11:31:26 -0500

    el comienzo en php

---
+++ Proyecto.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>proyecto Php</title>
+</head>
+<body>
+    <?php
+        echo "Que xopa mundo";
+    ?>
+</body>
+</html>
